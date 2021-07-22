60ea2180288aca21cc17bf2184e3e1c15d704d21
matthew diaz <mattdiaz0123@gmail.com>
2021-07-22 14:08:44 -0400

    pushing updatess

--- java/src/main/java/com/techelevator/Chips.java
+++ java/src/main/java/com/techelevator/Chips.java
@@ -2,7 +2,7 @@ package com.techelevator;
 
 public class Chips extends Product{
 
-    public Chips(String slotLocation, String name, double price, String category) {
-        super(slotLocation, name, price, category);
+    public Chips(String slotLocation, String name, double price, String category, int quantity) {
+        super(slotLocation, name, price, category, quantity);
     }
 }
--- java/src/main/java/com/techelevator/Drink.java
+++ java/src/main/java/com/techelevator/Drink.java
@@ -2,8 +2,8 @@ package com.techelevator;
 
 public class Drink extends Product{
 
-    public Drink(String slotLocation, String name, double price, String category) {
-        super(slotLocation, name, price, category);
+    public Drink(String slotLocation, String name, double price, String category, int quantity) {
+        super(slotLocation, name, price, category, quantity);
     }
 
 
